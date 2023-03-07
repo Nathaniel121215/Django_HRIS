e15b24e4952023bf9e22413f8b12101dfc7876cf
Nathaniel Peñaranda <53138333+Nathaniel121215@users.noreply.github.com>
2023-03-07 08:29:26 +0800

    url update

--- HRIS_PROJECT/HRIS_PROJECT/settings.py
+++ HRIS_PROJECT/HRIS_PROJECT/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'compressor',
+    'HRIS_APP',
 ]
 
 MIDDLEWARE = [
--- HRIS_PROJECT/tailwind.config.js
+++ HRIS_PROJECT/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      './templates/**/*.html',
+      './HRIS_APP/templates/**/*.html',
       './node_modules/flowbite/**/*.js',
 
   ],
